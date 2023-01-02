40ecd593b5b1450c99eda64b765fc8e690979c04
Kaushal Shirode <72267398+Krobo224@users.noreply.github.com>
2023-01-02 21:49:14 +0530

    Update data_kul.py

--- data_kul.py
+++ data_kul.py
@@ -15,74 +15,71 @@ import random
 
 #Data loader :
 sampling_rate = 128 #Hz
-window = 0.5 #sec 1, 0.1, 2
-overlapp = 0.6 # 0.8
+window = 1 #sec 1, 0.1, 2
+overlapp = 0.5 # 0.8
 window_size = math.ceil(sampling_rate*window)
 per_window_overlapp = 1
-overlap_status = False 
+overlap_status = True 
 
 if overlap_status == True:
     rem = 1 - overlapp
     per_window_overlapp = int(rem*window_size) # for getting to the first index of next window
 
-def divide_into_segments(window_size, per_window_overlapp, overlap_status, eeg_matrix, attended_ear_value, division_type):
+def divide_into_segments(window_size, per_window_overlapp, overlap_status, trial_train, trial_test, attended_ear_value):
     k=0
-    while k < eeg_matrix.shape[0] :
-        temp = eeg_matrix[k: k+window_size]
+    while k < trial_train.shape[0] :
+        temp = trial_train[k: k+window_size]
         # print("temp: ", temp.shape)
-        if temp.shape == (window_size, 64):
-            if division_type == 'train':
-                X_train.append(temp)
-                y_train.append(attended_ear_value)
-            elif division_type == 'val':
-                X_val.append(temp)
-                y_val.append(attended_ear_value)    
+        if temp.shape == (window_size, trial_train.shape[1]):
+            X_train.append(temp)
+            y_train.append(attended_ear_value)
+
         if overlap_status == True:
             k = k+per_window_overlapp    
         else:
             k = k+window_size
     
+    k=0
+    while k < trial_test.shape[0] :
+        temp = trial_test[k: k+window_size]
+        # print("temp: ", temp.shape)
+        if temp.shape == (window_size, trial_test.shape[1]):
+            X_test.append(temp)
+            y_test.append(attended_ear_value)
+
+        if overlap_status == True:
+            k = k+per_window_overlapp    
+        else:
+            k = k+window_size
     
 
 def read_eeg(data_path, fl, overlapp_status, per_window_overlapp):
     data = loadmat(data_path)
-    l1 = [0, 1, 2, 3, 4, 5, 6, 7] #trials
-    l2 = random.sample(range(0, 7), 5)
-    l3 = list(set(l1).symmetric_difference(l2))
-    for i in l2:
+    for i in range (8):
         eeg_matrix = data['preproc_trials'][0][i]['RawData'][0][0][0][0]['EegData'] # e.g. (12448, 64)
+        # print("EEG_MATRIX_SHAPE: ", eeg_matrix.shape)
         attended = data['preproc_trials'][0][i]['attended_ear'][0][0] #L/R
-  
-        #considering left attended value '0' and right attended value '1'
-      
-        attended_ear_value = 0
-        if(attended == 'R'):
-            attended_ear_value = 1
-        else:
-            attended_ear_value = 0
         
-        divide_into_segments(window_size, per_window_overlapp, overlap_status, eeg_matrix, attended_ear_value, 'train')
-
-        
-                
-    for i in l3:
-        eeg_matrix = data['preproc_trials'][0][i]['RawData'][0][0][0][0]['EegData'] # e.g. (12448, 64)
-        attended = data['preproc_trials'][0][i]['attended_ear'][0][0] #L/R
-  
-        #considering left attended value '0' and right attended value '1'
-      
         attended_ear_value = 0
-        if(attended == 'R'):
+        if attended == 'R':
             attended_ear_value = 1
-        else:
+        elif attended == 'L':
             attended_ear_value = 0
-
-        divide_into_segments(window_size, per_window_overlapp, overlap_status, eeg_matrix, attended_ear_value, 'val')
+            
+        train_index = int(eeg_matrix.shape[0]*0.8)
+        trial_train = eeg_matrix[:train_index]
+        trial_test = eeg_matrix[train_index:]
+        
+        # print("Train_trial", trial_train.shape)
+        # print("Test_trial", trial_test.shape)
+        
+        divide_into_segments(window_size, per_window_overlapp, overlap_status, trial_train, trial_test, attended_ear_value)
+        
 
 X_train = []
 y_train = []
-X_val = []
-y_val = []
+X_test = []
+y_test = []
 
 for i in range(16):
     print('======================================================')
@@ -95,8 +92,8 @@ for i in range(16):
     
 X_train = np.array(X_train)
 y_train = np.array(y_train)
-X_val = np.array(X_val)
-y_val = np.array(y_val)
+X_val = np.array(X_test)
+y_val = np.array(y_test)
 # print(y_val)
 
 
@@ -112,7 +109,7 @@ print("y_val shape: ", y_val.shape)
 print(y_train)
 
 
-# ##### Saving Numpy Arrays
+##### Saving Numpy Arrays
 np.savez_compressed('X_train.npz',np.array(X_train))
 np.savez_compressed('y_train.npz',np.array(y_train))
 np.savez_compressed('X_val.npz',np.array(X_val))
